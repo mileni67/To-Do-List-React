65d052b4a6c243f4df5d2ac6881dc6aa84fc5939
Oleksandr Vykhor <sashavihor67@gmail.com>
2023-05-19 20:06:19 +0200

    added theme (color, breakpoint)

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+    color: {
+        white: "#fff",
+        red: "#FF405B",
+        teal: "#008080",
+        silver: "#ccc",
+        alto: "#ddd"
+
+    },
+
+    breakpoint: {
+        mobileMax: 767,
+    },
+}
