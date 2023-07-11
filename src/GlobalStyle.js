ee8f526bfbe7185f246fd9461635a6493e7411ae
Oleksandr Vykhor <sashavihor67@gmail.com>
2023-07-11 18:25:48 +0200

    note the indentation in this code

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,15 +2,17 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     html {
-  box-sizing: border-box;
-}
+        box-sizing: border-box;
+    }
 
-*, ::after, ::before{
-  box-sizing: inherit;
-}
+    *,
+    ::after,
+    ::before {
+        box-sizing: inherit;
+    }
 
-body {
-  font-family: 'Monserrat', sans-serif;
-  background-color: ${({theme}) => theme.color.alto};
-}
+    body {
+        font-family: 'Monserrat', sans-serif;
+        background-color: ${({ theme }) => theme.color.alto};
+   }
 `;
